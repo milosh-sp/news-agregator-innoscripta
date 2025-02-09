deb90ff60877805f2787ce74a8e1478e16828883
Milosh <spasovskimilosf@gmail.com>
2025-02-09 11:33:08 +0100

    Minor organization

--- src/common/style/scss.d.ts
+++ src/common/style/scss.d.ts
@@ -7,3 +7,6 @@ declare module '*.scss' {
   const content: { [className: string]: string }
   export default content
 }
+
+//TODO: Generate types automatically for auto complete in the codebase
+// when scss is used in components
